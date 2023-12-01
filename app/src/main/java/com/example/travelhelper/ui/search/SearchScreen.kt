2f847e5d9b0876bdafa827d6e5c7b551f3896330
Seunggu Kang <alex9366@naver.com>
2023-12-01 11:15:40 +0900

    feat: 검색 화면 추가

---
+++ app/src/main/java/com/example/travelhelper/ui/search/SearchScreen.kt
@@ -0,0 +1,61 @@
+package com.example.travelhelper.ui.search
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.travelhelper.R
+import com.example.travelhelper.ui.designsystem.theme.TravelHelperTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun SearchScreen(navController: NavController) {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            bottomBar = {}
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize().padding(15.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxWidth()
+                        .height(200.dp)
+                        .padding(horizontal = 15.dp, vertical = 10.dp)
+                        .clip(MaterialTheme.shapes.large)
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.ic_alarm),
+                        contentDescription = "home_screen_bg",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+                Text(
+                    "Search Screen",
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(vertical = 20.dp)
+                )
+            }
+        }
+}
