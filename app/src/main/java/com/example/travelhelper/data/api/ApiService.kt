c5ed70da34cb14a379874938daef8f5182019265
Seunggu Kang <alex9366@naver.com>
2023-12-08 01:21:48 +0900

    feat: 근처 관광지, 관광지 순위 api 호출을 위한 함수 추가

---
+++ app/src/main/java/com/example/travelhelper/data/api/ApiService.kt
@@ -0,0 +1,31 @@
+package com.example.travelhelper.data.api
+
+import com.example.travelhelper.data.api.model.NearbyDestinationItem
+import com.example.travelhelper.data.api.model.PopularDestinationItem
+import com.example.travelhelper.data.api.model.TourApiResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("/B551011/DataLabService/locgoRegnVisitrDDList")
+    suspend fun getPopularDestination(
+        @Query("MobileOS") os: String = "AND",
+        @Query("MobileApp") serviceName: String = "TravelHelper",
+        @Query("_type") type: String = "json",
+        @Query("serviceKey") serviceKey: String = "AOUEIox1FTDxjysn7FGh2tBF4T3YJL6oCBus6k4pR2WhGBKO5ucIwiOVeCNdKBz13gL5JZ1IvCIYz3+OlG5wsQ==",
+        @Query("startYmd") startDate: String,
+        @Query("endYmd") endDate: String
+    ): TourApiResponse<PopularDestinationItem>
+
+    @GET("/B551011/Odii/themeLocationBasedList")
+    suspend fun getNearbyDestination(
+        @Query("MobileOS") os: String = "AND",
+        @Query("MobileApp") serviceName: String = "TravelHelper",
+        @Query("_type") type: String = "json",
+        @Query("serviceKey") serviceKey: String = "AOUEIox1FTDxjysn7FGh2tBF4T3YJL6oCBus6k4pR2WhGBKO5ucIwiOVeCNdKBz13gL5JZ1IvCIYz3+OlG5wsQ==",
+        @Query("mapX") x: String,
+        @Query("mapY") y: String,
+        @Query("radius") radius: String,
+        @Query("langCode") lan: String
+    ): TourApiResponse<NearbyDestinationItem>
+}
