5936d54ecc865a6fa3aad2da941270bf2fab94e5
Seunggu Kang <alex9366@naver.com>
2023-12-08 01:23:36 +0900

    feat: 관광지 dto 추가

---
+++ app/src/main/java/com/example/travelhelper/data/api/model/TourApiResponse.kt
@@ -0,0 +1,26 @@
+package com.example.travelhelper.data.api.model
+
+data class TourApiResponse<T>(
+    val response: Response<T>
+)
+
+data class Response<T>(
+    val header: Header,
+    val body: Body<T>
+)
+
+data class Header(
+    val resultCode: String,
+    val resultMsg: String
+)
+
+data class Body<T>(
+    val items: Items<T>,
+    val numOfRows: Int,
+    val pageNo: Int,
+    val totalCount: Int
+)
+
+data class Items<T>(
+    val item: List<T>
+)
