e476cc3f1c08f06cd523612a22caad39bd77356e
Seunggu Kang <alex9366@naver.com>
2023-12-08 01:25:06 +0900

    feat: usecase 주입을 위한 함수 추가

--- app/src/main/java/com/example/travelhelper/data/di/UseCaseModule.kt
+++ app/src/main/java/com/example/travelhelper/data/di/UseCaseModule.kt
@@ -1,7 +1,10 @@
 package com.example.travelhelper.data.di
 
+import com.example.travelhelper.data.repository.HomeRepository
 import com.example.travelhelper.data.repository.VisionRepository
-import com.example.travelhelper.domain.VisionUseCase
+import com.example.travelhelper.domain.usecase.GetNearbyDestinationUseCase
+import com.example.travelhelper.domain.usecase.GetPopularDestinationUseCase
+import com.example.travelhelper.domain.usecase.VisionUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -13,5 +16,15 @@ object UseCaseModule {
     @Provides
     fun provideVisionUseCase(visionRepository: VisionRepository): VisionUseCase {
         return VisionUseCase(visionRepository)
+    }
+
+    @Provides
+    fun provideGetPopularDestinationUseCase(homeRepository: HomeRepository): GetPopularDestinationUseCase {
+        return GetPopularDestinationUseCase(homeRepository)
+    }
+
+    @Provides
+    fun provideGetNearbyDestinationUseCase(homeRepository: HomeRepository): GetNearbyDestinationUseCase {
+        return GetNearbyDestinationUseCase(homeRepository)
     }
 }
