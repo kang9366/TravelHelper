de40993d6f8e9e8e31e433a017b861bfc988f102
Seunggu Kang <alex9366@naver.com>
2023-12-07 20:38:55 +0900

    feat: content resolver를 주입하기 위한 함수 추가

---
+++ app/src/main/java/com/example/travelhelper/data/di/AppModule.kt
@@ -0,0 +1,20 @@
+package com.example.travelhelper.data.di
+
+import android.content.ContentResolver
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideContentResolver(@ApplicationContext context: Context): ContentResolver {
+        return context.contentResolver
+    }
+}
