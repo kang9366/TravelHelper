edf97b75b42c612757b09e225008355a8115707e
Seunggu Kang <alex9366@naver.com>
2023-12-07 20:35:57 +0900

    update: Timber 초기화

--- app/src/main/java/com/example/travelhelper/TravelHelperApplication.kt
+++ app/src/main/java/com/example/travelhelper/TravelHelperApplication.kt
@@ -3,11 +3,13 @@ package com.example.travelhelper
 import android.app.Application
 import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class TravelHelperApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         FirebaseApp.initializeApp(this)
+        Timber.plant(Timber.DebugTree())
     }
 }
