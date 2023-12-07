2740bd60b3e375a843e3ec502b4d61a554a48528
Seunggu Kang <alex9366@naver.com>
2023-12-08 01:23:24 +0900

    feat: 관광지 순위 dto 추가

---
+++ app/src/main/java/com/example/travelhelper/data/api/model/PopularDestinationItem.kt
@@ -0,0 +1,12 @@
+package com.example.travelhelper.data.api.model
+
+data class PopularDestinationItem(
+    val baseYmd: String,
+    val daywkDivCd: String,
+    val daywkDivNm: String,
+    val signguCode: String,
+    val signguNm: String,
+    val touDivCd: String,
+    val touDivNm: String,
+    val touNum: String
+)
