0ffec5c1bb41550a69274e6d565acbad39033a32
Seunggu Kang <alex9366@naver.com>
2023-12-07 20:35:21 +0900

    update: navigator 경로 추가

--- app/src/main/java/com/example/travelhelper/ui/main/MainScreen.kt
+++ app/src/main/java/com/example/travelhelper/ui/main/MainScreen.kt
@@ -1,15 +1,18 @@
-package com.example.travelhelper
+package com.example.travelhelper.ui.main
 
 import android.annotation.SuppressLint
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.travelhelper.ui.bookmark.BookmarkScreen
 import com.example.travelhelper.ui.designsystem.component.BottomNavigationBar
 import com.example.travelhelper.ui.home.HomeScreen
@@ -17,17 +20,26 @@ import com.example.travelhelper.ui.navigation.BottomTab
 import com.example.travelhelper.ui.navigation.MainRoute
 import com.example.travelhelper.ui.navigation.navigateSearch
 import com.example.travelhelper.ui.search.SearchScreen
+import com.example.travelhelper.ui.vision.VisionScreen
+import java.net.URLDecoder
+import java.nio.charset.StandardCharsets
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
     val showBottomBar = remember { mutableStateOf(true) }
 
-    navController.addOnDestinationChangedListener { _, destination, _ ->
-        showBottomBar.value = destination.route != MainRoute.search
+    DisposableEffect(navController) {
+        val listener = NavController.OnDestinationChangedListener { _, destination, _ ->
+            showBottomBar.value = destination.route !in listOf(MainRoute.search, MainRoute.vision)
+        }
+        navController.addOnDestinationChangedListener(listener)
+        onDispose {
+            navController.removeOnDestinationChangedListener(listener)
+        }
     }
+
 
     Scaffold(
         bottomBar = { if (showBottomBar.value) BottomNavigationBar(navController) },
@@ -52,7 +64,20 @@ fun Navigation(navController: NavHostController) {
         }
 
         composable(MainRoute.search) {
-            SearchScreen(navController = navController)
+            SearchScreen(
+                onBackClick = navController::popBackStack
+            )
+        }
+
+        composable(
+            route = "${MainRoute.vision}/{imageUri}",
+            arguments = listOf(navArgument("imageUri") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val imageUri = backStackEntry.arguments?.getString("imageUri")
+            imageUri?.let { VisionScreen(
+                URLDecoder.decode(it, StandardCharsets.UTF_8.toString()),
+                onBackClick = navController::popBackStack
+            ) }
         }
     }
 }
