07bc3f6f77abbd1031a8d1ca65e271c8f0a674f0
Seunggu Kang <alex9366@naver.com>
2023-12-07 20:35:06 +0900

    update: navigator 경로 추가

--- app/src/main/java/com/example/travelhelper/ui/navigation/MainNavigation.kt
+++ app/src/main/java/com/example/travelhelper/ui/navigation/MainNavigation.kt
@@ -6,7 +6,12 @@ fun NavController.navigateSearch() {
     navigate(MainRoute.search)
 }
 
+fun NavController.navigateVision(imageUri: String) {
+    navigate("${MainRoute.vision}/$imageUri")
+}
+
 object MainRoute {
     const val search = "search"
+    const val vision = "vision"
     const val detail = "detail"
 }
