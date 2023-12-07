3a399e5aa4e9ac24963abdc04e022034c8978404
Seunggu Kang <alex9366@naver.com>
2023-12-07 20:32:03 +0900

    feat: 저장 목록 화면 추가

--- app/src/main/java/com/example/travelhelper/ui/bookmark/BookmarkScreen.kt
+++ app/src/main/java/com/example/travelhelper/ui/bookmark/BookmarkScreen.kt
@@ -1,55 +1,42 @@
 package com.example.travelhelper.ui.bookmark
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.travelhelper.R
+import com.example.travelhelper.ui.designsystem.component.BookmarkContent
+import com.example.travelhelper.ui.designsystem.component.TopAppBarNavigationType
+import com.example.travelhelper.ui.designsystem.component.TravelHelperTopBar
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun BookmarkScreen(navController: NavController) {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
+    Scaffold(
+        topBar = { TravelHelperTopBar(navigationType = TopAppBarNavigationType.BOOKMARK) },
     ) {
-        Column(
-            modifier = Modifier.fillMaxSize().padding(15.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 48.dp)
+                .background(Color(0xFFF5F7FC))
         ) {
-            Box(
-                modifier = Modifier.fillMaxWidth()
-                    .height(200.dp)
-                    .padding(horizontal = 15.dp, vertical = 10.dp)
-                    .clip(MaterialTheme.shapes.large)
-            ) {
-                Image(
-                    painter = painterResource(R.drawable.ic_alarm),
-                    contentDescription = "home_screen_bg",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier.fillMaxSize()
+            items(100) {
+                Spacer(modifier = Modifier.height(16.dp))
+                BookmarkContent(
+                    onClick = {
+
+                    }
                 )
             }
-            Text(
-                text = "Home Screen",
-                style = MaterialTheme.typography.titleLarge,
-                modifier = Modifier.padding(vertical = 20.dp)
-            )
         }
     }
 }
