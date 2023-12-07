1a7c4c6ddddb1872ffc3cb721d11fa2c66bfb341
Seunggu Kang <alex9366@naver.com>
2023-12-08 01:26:36 +0900

    update: 주석 제거

--- app/src/main/java/com/example/travelhelper/ui/vision/VisionViewModel.kt
+++ app/src/main/java/com/example/travelhelper/ui/vision/VisionViewModel.kt
@@ -1,29 +1,18 @@
 package com.example.travelhelper.ui.vision
 
-import android.content.ContentResolver
-import android.graphics.Bitmap
-import android.graphics.ImageDecoder
 import android.net.Uri
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.travelhelper.domain.VisionUseCase
-import com.google.firebase.Firebase
-import com.google.firebase.functions.FirebaseFunctions
-import com.google.firebase.functions.functions
-import com.google.firebase.ml.vision.FirebaseVision
-import com.google.firebase.ml.vision.cloud.FirebaseVisionCloudDetectorOptions
-import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.example.travelhelper.domain.usecase.VisionUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -32,8 +21,9 @@ class VisionViewModel
     private val visionUseCase: VisionUseCase
 ): ViewModel() {
     private val _imageUri = MutableStateFlow<Uri?>(null)
+    @OptIn(ExperimentalCoroutinesApi::class)
     val imageAnalysisResult: StateFlow<String> = _imageUri
-        .filterNotNull() // null이 아닌 URI만 처리
+        .filterNotNull()
         .flatMapLatest { uri ->
             flow {
                 val result = visionUseCase(uri) // VisionUseCase 호출
