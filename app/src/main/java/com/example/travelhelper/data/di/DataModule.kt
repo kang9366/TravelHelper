14ad605d6e4beb7bcb1558ee08a8d7a610748764
Seunggu Kang <alex9366@naver.com>
2023-12-08 01:25:19 +0900

    feat: repository 주입을 위한 함수 추가

--- app/src/main/java/com/example/travelhelper/data/di/DataModule.kt
+++ app/src/main/java/com/example/travelhelper/data/di/DataModule.kt
@@ -1,6 +1,9 @@
 package com.example.travelhelper.data.di
 
 import android.content.ContentResolver
+import com.example.travelhelper.data.api.ApiService
+import com.example.travelhelper.data.repository.HomeRepository
+import com.example.travelhelper.data.repository.HomeRepositoryImpl
 import com.example.travelhelper.data.repository.VisionRepository
 import com.example.travelhelper.data.repository.VisionRepositoryImpl
 import dagger.Module
@@ -16,5 +19,11 @@ class DataModule {
     @Singleton
     fun provideVisionRepository(contentResolver: ContentResolver): VisionRepository {
         return VisionRepositoryImpl(contentResolver)
+    }
+
+    @Provides
+    @Singleton
+    fun provideHomeRepository(apiService: ApiService): HomeRepository {
+        return HomeRepositoryImpl(apiService)
     }
 }
