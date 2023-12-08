5b77f2363de304d4cc7d6ca1b516a86e0e88d1be
Seunggu Kang <alex9366@naver.com>
2023-12-08 23:30:11 +0900

    update: 상세 정보 화면으로 이동하기 위한 확장함수 추가

--- app/src/main/java/com/example/travelhelper/ui/navigation/MainNavigation.kt
+++ app/src/main/java/com/example/travelhelper/ui/navigation/MainNavigation.kt
@@ -10,6 +10,10 @@ fun NavController.navigateVision(imageUri: String) {
     navigate("${MainRoute.vision}/$imageUri")
 }
 
+fun NavController.navigateDetail(title: String) {
+    navigate("${MainRoute.detail}/$title")
+}
+
 object MainRoute {
     const val search = "search"
     const val vision = "vision"
