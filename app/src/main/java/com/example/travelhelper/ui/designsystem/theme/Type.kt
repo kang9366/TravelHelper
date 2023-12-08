76b5bdd5d2b0fd8679652cefc2031600f5d81fed
Seunggu Kang <alex9366@naver.com>
2023-12-08 23:25:20 +0900

    update: 폰트 수정

--- app/src/main/java/com/example/travelhelper/ui/designsystem/theme/Type.kt
+++ app/src/main/java/com/example/travelhelper/ui/designsystem/theme/Type.kt
@@ -8,28 +8,46 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.example.travelhelper.R
 
-val fonts = FontFamily(
-    Font(R.font.pretendard_bold),
-    Font(R.font.pretendard_regular)
-)
-// Set of Material typography styles to start with
+val font = FontFamily(Font(R.font.technasans_regular))
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = fonts,
-        fontWeight = FontWeight.Normal,
+        fontFamily = font,
+        fontWeight = FontWeight.Bold,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
+    bodyMedium = TextStyle(
+        fontFamily = font,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
     titleLarge = TextStyle(
-        fontFamily = fonts,
+        fontFamily = font,
         fontWeight = FontWeight.Normal,
         fontSize = 24.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
+    titleMedium = TextStyle(
+        fontFamily = font,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = font,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
     labelSmall = TextStyle(
-        fontFamily = fonts,
+        fontFamily = font,
         fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
         lineHeight = 16.sp,
