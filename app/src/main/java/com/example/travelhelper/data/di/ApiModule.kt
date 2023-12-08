0965dd7410fe91e4066c6040bcb4322a91844c32
Seunggu Kang <alex9366@naver.com>
2023-12-08 23:27:36 +0900

    update: 공공데이터 api와 이미지 검색 api 호출을 위한 base url을 다르게 설정 및 다른 ApiService를 주입할 수 있도록 수정

--- app/src/main/java/com/example/travelhelper/data/di/ApiModule.kt
+++ app/src/main/java/com/example/travelhelper/data/di/ApiModule.kt
@@ -1,6 +1,7 @@
 package com.example.travelhelper.data.di
 
-import com.example.travelhelper.data.api.ApiService
+import com.example.travelhelper.data.api.ImageApiService
+import com.example.travelhelper.data.api.TourApiService
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
@@ -17,7 +18,8 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 internal object ApiModule {
-    private const val BASE_URL = "https://apis.data.go.kr"
+    private const val TOUR_BASE_URL = "https://apis.data.go.kr"
+    private const val IMAGE_BASE_URL = "https://openapi.naver.com"
 
     @Provides
     @Singleton
@@ -43,14 +45,27 @@ internal object ApiModule {
 
     @Provides
     @Singleton
-    fun provideApiService(
+    fun provideTourApiService(
         okHttpClient: OkHttpClient,
         moshi: Moshi
-    ): ApiService {
+    ): TourApiService {
         return Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(TOUR_BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .client(okHttpClient).build()
-            .create(ApiService::class.java)
+            .create(TourApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideImageApiService(
+        okHttpClient: OkHttpClient,
+        moshi: Moshi
+    ): ImageApiService {
+        return Retrofit.Builder()
+            .baseUrl(IMAGE_BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(okHttpClient).build()
+            .create(ImageApiService::class.java)
     }
 }
