43851f92e1c326c97318631d0910813f0741e08a
Seunggu Kang <alex9366@naver.com>
2023-12-08 23:17:44 +0900

    update: 관광지 상세 정보 entity 추가

---
+++ app/src/main/java/com/example/travelhelper/data/model/DestinationDetail.kt
@@ -0,0 +1,9 @@
+package com.example.travelhelper.data.model
+
+data class DestinationDetail(
+    val name: String,
+    val address: String,
+    val tel: String,
+    val x: Float,
+    val y: Float
+)
