ff0c7c09d9cadb85e709b227eca75c439549cb24
Seunggu Kang <alex9366@naver.com>
2023-12-08 23:21:07 +0900

    feat: 근처 관광지 mapper 추가

---
+++ app/src/main/java/com/example/travelhelper/data/mapper/NearbyDestinationMapper.kt
@@ -0,0 +1,13 @@
+package com.example.travelhelper.data.mapper
+
+import com.example.travelhelper.data.api.model.NearbyDestinationItem
+import com.example.travelhelper.data.model.NearbyDestination
+
+fun NearbyDestinationItem.toData(): NearbyDestination {
+    return NearbyDestination(
+        name = title,
+        address = "$addr1 $addr2",
+        x = mapX,
+        y = mapY
+    )
+}
