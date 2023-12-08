8c0ea581353b9ca7c4e27bc3897ce606a62b7e10
Seunggu Kang <alex9366@naver.com>
2023-12-08 23:25:14 +0900

    update: 인터페이스 이름 수정 및 상세 정보를 받아오기 위한 api 함수 추가

--- app/src/main/java/com/example/travelhelper/data/api/TourApiService.kt
+++ app/src/main/java/com/example/travelhelper/data/api/TourApiService.kt
@@ -3,10 +3,11 @@ package com.example.travelhelper.data.api
 import com.example.travelhelper.data.api.model.NearbyDestinationItem
 import com.example.travelhelper.data.api.model.PopularDestinationItem
 import com.example.travelhelper.data.api.model.TourApiResponse
+import com.example.travelhelper.data.api.model.TourDetailItem
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface ApiService {
+interface TourApiService {
     @GET("/B551011/DataLabService/locgoRegnVisitrDDList")
     suspend fun getPopularDestination(
         @Query("MobileOS") os: String = "AND",
@@ -28,4 +29,13 @@ interface ApiService {
         @Query("radius") radius: String,
         @Query("langCode") lan: String
     ): TourApiResponse<NearbyDestinationItem>
+
+    @GET("/B551011/EngService1/searchKeyword1")
+    suspend fun getDestinationDetail(
+        @Query("MobileOS") os: String = "AND",
+        @Query("MobileApp") serviceName: String = "TravelHelper",
+        @Query("_type") type: String = "json",
+        @Query("serviceKey") serviceKey: String = "AOUEIox1FTDxjysn7FGh2tBF4T3YJL6oCBus6k4pR2WhGBKO5ucIwiOVeCNdKBz13gL5JZ1IvCIYz3+OlG5wsQ==",
+        @Query("keyword") query: String
+    ): TourApiResponse<TourDetailItem>
 }
