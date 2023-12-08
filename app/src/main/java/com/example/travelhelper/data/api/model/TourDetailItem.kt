2c0582ac20812ba9e929f33caab5982fc9728a3e
Seunggu Kang <alex9366@naver.com>
2023-12-08 23:20:09 +0900

    feat: 상세 정보 dto 추가

---
+++ app/src/main/java/com/example/travelhelper/data/api/model/TourDetailItem.kt
@@ -0,0 +1,23 @@
+package com.example.travelhelper.data.api.model
+
+data class TourDetailItem(
+    val addr1: String,
+    val addr2: String,
+    val areacode: String,
+    val cat1: String,
+    val cat2: String,
+    val cat3: String,
+    val contentid: String,
+    val contenttypeid: String,
+    val createdtime: String,
+    val firstimage: String,
+    val firstimage2: String,
+    val cpyrhtDivCd: String,
+    val mapx: Float,
+    val mapy: Float,
+    val mlevel: String,
+    val modifiedtime: String,
+    val sigungucode: String,
+    val tel: String,
+    val title: String
+)
