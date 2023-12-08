c68c345a6f575ec7decb6b6f4edf09699cb00f26
Seunggu Kang <alex9366@naver.com>
2023-12-08 23:17:53 +0900

    update: 관광지 상세 정보 mapper 추가

---
+++ app/src/main/java/com/example/travelhelper/data/mapper/DestinationDetailMapper.kt
@@ -0,0 +1,14 @@
+package com.example.travelhelper.data.mapper
+
+import com.example.travelhelper.data.api.model.TourDetailItem
+import com.example.travelhelper.data.model.DestinationDetail
+
+fun TourDetailItem.toData(): DestinationDetail {
+    return DestinationDetail(
+        name = title,
+        address = "$addr1 $addr2",
+        tel = tel,
+        x = mapx,
+        y = mapy
+    )
+}
