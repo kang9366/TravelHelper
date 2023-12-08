0535a9809def523e48e14869dbf855fc5f6cdbe4
Seunggu Kang <alex9366@naver.com>
2023-12-08 23:24:46 +0900

    update: usecase 인터페이스를 상속받도록 수정

--- app/src/main/java/com/example/travelhelper/domain/usecase/GetNearbyDestinationUseCase.kt
+++ app/src/main/java/com/example/travelhelper/domain/usecase/GetNearbyDestinationUseCase.kt
@@ -1,14 +1,13 @@
 package com.example.travelhelper.domain.usecase
 
-import com.example.travelhelper.data.api.model.NearbyDestinationItem
-import com.example.travelhelper.data.api.model.TourApiResponse
-import com.example.travelhelper.data.repository.HomeRepository
+import com.example.travelhelper.data.model.NearbyDestination
+import com.example.travelhelper.domain.repository.HomeRepository
 import javax.inject.Inject
 
 class GetNearbyDestinationUseCase @Inject constructor(
     private val homeRepository: HomeRepository
-) {
-    suspend operator fun invoke(x: String, y: String, radius: String, lan: String): TourApiResponse<NearbyDestinationItem> {
+): UseCase {
+    suspend operator fun invoke(x: String, y: String, radius: String, lan: String): List<NearbyDestination> {
         return homeRepository.getNearbyDestination(x, y, radius, lan)
     }
 }
--- app/src/main/java/com/example/travelhelper/domain/usecase/GetPopularDestinationUseCase.kt
+++ app/src/main/java/com/example/travelhelper/domain/usecase/GetPopularDestinationUseCase.kt
@@ -1,15 +1,14 @@
 package com.example.travelhelper.domain.usecase
 
-import android.net.Uri
-import com.example.travelhelper.data.api.model.PopularDestinationItem
-import com.example.travelhelper.data.api.model.TourApiResponse
-import com.example.travelhelper.data.repository.HomeRepository
+import com.example.travelhelper.data.model.PopularDestination
+import com.example.travelhelper.domain.repository.HomeRepository
 import javax.inject.Inject
 
 class GetPopularDestinationUseCase @Inject constructor(
     private val homeRepository: HomeRepository
-) {
-    suspend operator fun invoke(startDate: String, endDate: String): TourApiResponse<PopularDestinationItem> {
+): UseCase {
+    suspend operator fun invoke(startDate: String, endDate: String): List<PopularDestination> {
         return homeRepository.getPopularDestination(startDate, endDate)
+            .distinctBy { it.name }
     }
 }
