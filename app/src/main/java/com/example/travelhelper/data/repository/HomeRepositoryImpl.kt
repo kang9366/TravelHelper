70eea2d944a2ce1045d5a72b45849c4090a78eef
Seunggu Kang <alex9366@naver.com>
2023-12-08 23:28:53 +0900

    update: mapper를 이용해 데이터 가공

--- app/src/main/java/com/example/travelhelper/data/repository/HomeRepositoryImpl.kt
+++ app/src/main/java/com/example/travelhelper/data/repository/HomeRepositoryImpl.kt
@@ -1,19 +1,39 @@
 package com.example.travelhelper.data.repository
 
-import com.example.travelhelper.data.api.ApiService
-import com.example.travelhelper.data.api.model.NearbyDestinationItem
-import com.example.travelhelper.data.api.model.PopularDestinationItem
-import com.example.travelhelper.data.api.model.TourApiResponse
+import com.example.travelhelper.data.api.ImageApiService
+import com.example.travelhelper.data.api.TourApiService
+import com.example.travelhelper.data.mapper.toData
+import com.example.travelhelper.data.model.DestinationImage
+import com.example.travelhelper.data.model.NearbyDestination
+import com.example.travelhelper.data.model.PopularDestination
+import com.example.travelhelper.domain.repository.HomeRepository
 import javax.inject.Inject
 
 class HomeRepositoryImpl @Inject constructor(
-    private val api: ApiService
+    private val tourApi: TourApiService,
+    private val imageApi: ImageApiService
 ): HomeRepository {
-    override suspend fun getPopularDestination(startDate: String, endDate: String): TourApiResponse<PopularDestinationItem> {
-        return api.getPopularDestination(startDate = startDate, endDate = endDate)
+    override suspend fun getPopularDestination(startDate: String, endDate: String): List<PopularDestination> {
+        return tourApi.getPopularDestination(startDate = startDate, endDate = endDate)
+            .response
+            .body
+            .items
+            .item
+            .map { it.toData() }
     }
 
-    override suspend fun getNearbyDestination(x: String, y: String, radius: String, lan: String): TourApiResponse<NearbyDestinationItem> {
-        return api.getNearbyDestination(x = x, y = y, radius = radius, lan = lan)
+    override suspend fun getNearbyDestination(x: String, y: String, radius: String, lan: String): List<NearbyDestination> {
+        return tourApi.getNearbyDestination(x = x, y = y, radius = radius, lan = lan)
+            .response
+            .body
+            .items
+            .item
+            .map { it.toData() }
+    }
+
+    override suspend fun getImage(query: String, count: Int): List<DestinationImage> {
+        return imageApi.getImage(query = query, count = count).items.map {
+            it.toData()
+        }
     }
 }
