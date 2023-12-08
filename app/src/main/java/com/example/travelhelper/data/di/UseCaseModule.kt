35cd26888485ccd72b7ad413c430a0ce82b490ba
Seunggu Kang <alex9366@naver.com>
2023-12-08 23:25:32 +0900

    update: 새롭게 추가된 usecase를 주입하기 위한 함수 추가

--- app/src/main/java/com/example/travelhelper/data/di/UseCaseModule.kt
+++ app/src/main/java/com/example/travelhelper/data/di/UseCaseModule.kt
@@ -1,7 +1,10 @@
 package com.example.travelhelper.data.di
 
-import com.example.travelhelper.data.repository.HomeRepository
-import com.example.travelhelper.data.repository.VisionRepository
+import com.example.travelhelper.domain.repository.DetailRepository
+import com.example.travelhelper.domain.repository.HomeRepository
+import com.example.travelhelper.domain.repository.VisionRepository
+import com.example.travelhelper.domain.usecase.GetDestinationDetailUseCase
+import com.example.travelhelper.domain.usecase.GetImageUseCase
 import com.example.travelhelper.domain.usecase.GetNearbyDestinationUseCase
 import com.example.travelhelper.domain.usecase.GetPopularDestinationUseCase
 import com.example.travelhelper.domain.usecase.VisionUseCase
@@ -26,5 +29,15 @@ object UseCaseModule {
     @Provides
     fun provideGetNearbyDestinationUseCase(homeRepository: HomeRepository): GetNearbyDestinationUseCase {
         return GetNearbyDestinationUseCase(homeRepository)
+    }
+
+    @Provides
+    fun provideGetImageUseCase(homeRepository: HomeRepository): GetImageUseCase {
+        return GetImageUseCase(homeRepository)
+    }
+
+    @Provides
+    fun provideGetDestinationDetailUseCase(detailRepository: DetailRepository): GetDestinationDetailUseCase {
+        return GetDestinationDetailUseCase(detailRepository)
     }
 }
