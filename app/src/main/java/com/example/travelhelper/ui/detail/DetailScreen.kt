ade4ab02b6bd109b7e6bbd777202beac210ff972
Seunggu Kang <alex9366@naver.com>
2023-12-09 22:12:43 +0900

    feat: 구글맵 추가 및 결과가 없는 경우에 alert dialog 추가

--- app/src/main/java/com/example/travelhelper/ui/detail/DetailScreen.kt
+++ app/src/main/java/com/example/travelhelper/ui/detail/DetailScreen.kt
@@ -1,15 +1,19 @@
 package com.example.travelhelper.ui.detail
 
 import android.annotation.SuppressLint
-import android.util.Log
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -20,19 +24,27 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.travelhelper.R
+import com.example.travelhelper.ui.designsystem.component.AlertDialog
 import com.example.travelhelper.ui.designsystem.component.ImageSlider
+import com.example.travelhelper.ui.designsystem.component.Loading
 import com.example.travelhelper.ui.designsystem.component.TopAppBarNavigationType
 import com.example.travelhelper.ui.designsystem.component.TravelHelperTopBar
 import com.example.travelhelper.ui.designsystem.theme.TravelHelperTheme
 import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 
-@OptIn(ExperimentalPagerApi::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun DetailScreen(
@@ -41,6 +53,7 @@ fun DetailScreen(
     viewModel: DetailViewModel = hiltViewModel()
 ) {
     val destinationDetailUiState by viewModel.destinationDetailUiState.collectAsState()
+
     LaunchedEffect(key1 = Unit) {
         viewModel.fetchDestinationDetail(title)
     }
@@ -61,16 +74,24 @@ fun DetailScreen(
         ) {
             Spacer(modifier = Modifier.height(68.dp))
 
-            Content(destinationDetailUiState)
+            DetailContent(destinationDetailUiState, onBackClick)
         }
     }
 }
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
-private fun Content(uiState: DestinationDetailUiState) {
+fun DetailContent(
+    uiState: DestinationDetailUiState,
+    onBackClick: () -> Unit
+) {
     when(uiState) {
         is DestinationDetailUiState.DestinationDetails -> {
+            val location = LatLng(uiState.destinationDetail.y, uiState.destinationDetail.x)
+            val cameraPositionState = rememberCameraPositionState {
+                position = CameraPosition.fromLatLngZoom(location, 15f)
+            }
+
             Column {
                 ImageSlider(
                     imageList = uiState.image
@@ -83,13 +104,59 @@ private fun Content(uiState: DestinationDetailUiState) {
                 Information(iconRes = R.drawable.ic_location, description = uiState.destinationDetail.address)
                 Spacer(modifier = Modifier.height(13.dp))
                 Information(iconRes = R.drawable.ic_tel, description = uiState.destinationDetail.tel)
+                GoogleMap(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(220.dp)
+                        .clip(RoundedCornerShape(12.dp)),
+                    cameraPositionState = cameraPositionState
+                ) {
+                    Marker(
+                        state = MarkerState(position = location),
+                        title = uiState.destinationDetail.name
+                    )
+                }
             }
         }
         is DestinationDetailUiState.Loading -> {
-            Text("로딩중")
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Loading(modifier = Modifier)
+            }
         }
         is DestinationDetailUiState.Empty -> {
-            Text("empty")
+            AlertDialog(onDismissRequest = onBackClick) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(230.dp)
+                        .padding(horizontal = 10.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                        .background(Color.White),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Spacer(modifier = Modifier.height(85.dp))
+                    Text(
+                        text = "There is no data in the public data portal",
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    Button(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 20.dp)
+                            .clip(RoundedCornerShape(16.dp)),
+                        onClick = onBackClick
+                    ) {
+                        Text(
+                            text = "OK",
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(15.dp))
+                }
+            }
         }
     }
 }
