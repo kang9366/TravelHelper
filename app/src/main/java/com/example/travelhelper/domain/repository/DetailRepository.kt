777c73e4e2428df8c9fb5db8692e2b480a31df3d
Seunggu Kang <alex9366@naver.com>
2023-12-15 20:34:53 +0900

    feat: 관광지에 대한 설명을 요청하는 함수 추가

--- app/src/main/java/com/example/travelhelper/domain/repository/DetailRepository.kt
+++ app/src/main/java/com/example/travelhelper/domain/repository/DetailRepository.kt
@@ -1,7 +1,10 @@
 package com.example.travelhelper.domain.repository
 
+import com.example.travelhelper.data.remote.model.ChatGptRequest
+import com.example.travelhelper.data.remote.model.GptResponse
 import com.example.travelhelper.domain.entity.DestinationDetail
 
 interface DetailRepository {
     suspend fun getDestinationDetail(query: String): DestinationDetail
+    suspend fun getDestinationInformation(request: ChatGptRequest): GptResponse
 }
