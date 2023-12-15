e8a18bd623b3c9fecfcf09743f4de0bec188dec8
Seunggu Kang <alex9366@naver.com>
2023-12-15 20:43:51 +0900

    feat: Detail 화면에서 사용할 TopBar 추가

--- app/src/main/java/com/example/travelhelper/ui/component/TopBar.kt
+++ app/src/main/java/com/example/travelhelper/ui/component/TopBar.kt
@@ -2,6 +2,8 @@ package com.example.travelhelper.ui.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -11,23 +13,30 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.travelhelper.R
+import com.example.travelhelper.ui.theme.TravelHelperTheme
 
-enum class TopAppBarNavigationType { BACK, HOME, BOOKMARK }
+enum class TopAppBarNavigationType { BACK, HOME, BOOKMARK, DETAIL }
 
 @Composable
 fun TravelHelperTopBar(
     navigationType: TopAppBarNavigationType,
-    onNavigationClick: () -> Unit = {}
+    onNavigationClick: () -> Unit = {},
+    onSaveClick: () -> Unit = {},
 ) {
     Box(
-        modifier = Modifier.fillMaxWidth().height(48.dp).background(Color.White)
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp)
+            .background(Color.White)
     ) {
         when(navigationType) {
             TopAppBarNavigationType.BACK -> {
@@ -41,14 +50,49 @@ fun TravelHelperTopBar(
                     )
                 }
             }
+            TopAppBarNavigationType.DETAIL -> {
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    IconButton(onClick = onNavigationClick, modifier = Modifier.size(48.dp)) {
+                        Icon(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(vertical = 12.dp, horizontal = 12.dp),
+                            painter = painterResource(id = R.drawable.ic_left),
+                            contentDescription = null
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    IconButton(onClick = onSaveClick, modifier = Modifier.size(48.dp)) {
+                        Icon(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(vertical = 12.dp, horizontal = 12.dp),
+                            painter = painterResource(id = R.drawable.ic_bookmark),
+                            tint = MaterialTheme.colorScheme.primary,
+                            contentDescription = null
+                        )
+                    }
+                }
+            }
             TopAppBarNavigationType.BOOKMARK -> {
-                Text(
-                    text = "Bookmark",
-                    style = MaterialTheme.typography.titleLarge,
-                    modifier = Modifier
-                        .align(Alignment.CenterStart)
-                        .padding(horizontal = 20.dp, vertical = 10.dp)
-                )
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(
+                        text = "Bookmark",
+                        style = MaterialTheme.typography.titleLarge,
+                        modifier = Modifier
+                            .padding(horizontal = 20.dp, vertical = 10.dp)
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    TextButton(onClick = { onNavigationClick() }) {
+                        Text("Clear All")
+                    }
+                }
             }
             TopAppBarNavigationType.HOME -> {
                 Text(
@@ -60,5 +104,15 @@ fun TravelHelperTopBar(
                 )
             }
         }
+    }
+}
+
+@Preview
+@Composable
+private fun TravelHelperTopAppBarTest() {
+    TravelHelperTheme {
+        TravelHelperTopBar(
+            navigationType = TopAppBarNavigationType.DETAIL
+        )
     }
 }
