cdaa3e807b6d167fcf006bae0ba952b552afbbd0
Seunggu Kang <alex9366@naver.com>
2023-12-15 20:34:33 +0900

    update: 수정된 경로 반영

--- app/src/main/java/com/example/travelhelper/data/mapper/DestinationImageMapper.kt
+++ app/src/main/java/com/example/travelhelper/data/mapper/DestinationImageMapper.kt
@@ -1,6 +1,6 @@
 package com.example.travelhelper.data.mapper
 
-import com.example.travelhelper.data.api.model.ImageItem
+import com.example.travelhelper.data.remote.model.ImageItem
 import com.example.travelhelper.domain.entity.DestinationImage
 
 fun ImageItem.toData(): DestinationImage {
