b5e454c28a2dafe24882b382ad285b459ba0e519
Seunggu Kang <alex9366@naver.com>
2023-12-15 20:31:14 +0900

    feat: Gpt Response DTO 추가

---
+++ app/src/main/java/com/example/travelhelper/data/remote/model/GptResponse.kt
@@ -0,0 +1,28 @@
+package com.example.travelhelper.data.remote.model
+
+data class GptResponse(
+    val id: String,
+    val `object`: String,
+    val created: Long,
+    val model: String,
+    val choices: List<Choice>,
+    val usage: Usage,
+    val system_fingerprint: String?
+)
+
+data class Choice(
+    val index: Int,
+    val message: Message,
+    val finish_reason: String
+)
+
+data class Message(
+    val role: String,
+    val content: String
+)
+
+data class Usage(
+    val prompt_tokens: Int,
+    val completion_tokens: Int,
+    val total_tokens: Int
+)
