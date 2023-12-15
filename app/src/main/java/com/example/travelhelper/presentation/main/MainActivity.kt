cea0f6bfa75c0420823d0db3962c2d6b4fead503
Seunggu Kang <alex9366@naver.com>
2023-12-15 20:43:24 +0900

    feat: 사용자에게 위치 권한 체크 및 전화화면으로 이동하는 함수 구현

--- app/src/main/java/com/example/travelhelper/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/travelhelper/presentation/main/MainActivity.kt
@@ -1,21 +1,44 @@
 package com.example.travelhelper.presentation.main
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.travelhelper.ui.theme.TravelHelperTheme
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationTokenSource
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private var cancellationTokenSource: CancellationTokenSource? = null
+
+    companion object {
+        private const val REQUEST_ACCESS_LOCATION_PERMISSION = 100
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
+
+        requestLocationPermission()
 
         setContent {
             TravelHelperTheme {
@@ -23,9 +46,53 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainScreen()
+                    MainScreen(
+                        callTel = { test(it) }
+                    )
                 }
             }
         }
+    }
+
+    private fun requestLocationPermission(){
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            REQUEST_ACCESS_LOCATION_PERMISSION
+        )
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        val locationPermissionGranted =
+            requestCode == REQUEST_ACCESS_LOCATION_PERMISSION
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+
+        if(locationPermissionGranted){
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+            cancellationTokenSource = CancellationTokenSource()
+            fusedLocationProviderClient.getCurrentLocation(
+                LocationRequest.PRIORITY_HIGH_ACCURACY,
+                cancellationTokenSource!!.token
+            ).addOnSuccessListener {
+                Log.d("tesw", "ss" + it.altitude.toString())
+                Log.d("tesw", "st" + it.longitude.toString())
+            }
+        } else {
+            Toast.makeText(this, "Please allow permission", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun test(number: String) {
+        val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$number"))
+        startActivity(intent)
     }
 }
