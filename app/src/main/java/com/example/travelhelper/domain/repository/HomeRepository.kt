5a96004efdcc943884cdd4676542e8e5d6e816c5
Seunggu Kang <alex9366@naver.com>
2023-12-15 20:41:26 +0900

    feat: 환율 정보 요청을 위한 함수 추가

--- app/src/main/java/com/example/travelhelper/domain/repository/HomeRepository.kt
+++ app/src/main/java/com/example/travelhelper/domain/repository/HomeRepository.kt
@@ -1,5 +1,6 @@
 package com.example.travelhelper.domain.repository
 
+import com.example.travelhelper.data.remote.model.CurrencyResponse
 import com.example.travelhelper.domain.entity.DestinationImage
 import com.example.travelhelper.domain.entity.NearbyDestination
 import com.example.travelhelper.domain.entity.PopularDestination
@@ -8,4 +9,5 @@ interface HomeRepository {
     suspend fun getPopularDestination(startDate: String, endDate: String): List<PopularDestination>
     suspend fun getNearbyDestination(x: String, y: String, radius: String, lan: String): List<NearbyDestination>
     suspend fun getImage(query: String, count: Int): List<DestinationImage>
+    suspend fun getCurrency(): List<CurrencyResponse>
 }
