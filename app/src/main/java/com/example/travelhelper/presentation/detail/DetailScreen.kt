007ab6877fed7fe4067d5baaf842c5883ebbbee3
Seunggu Kang <alex9366@naver.com>
2023-12-15 20:35:31 +0900

    update: screen을 scroll 할 수 있도록 수정

--- app/src/main/java/com/example/travelhelper/presentation/detail/DetailScreen.kt
+++ app/src/main/java/com/example/travelhelper/presentation/detail/DetailScreen.kt
@@ -3,6 +3,7 @@ package com.example.travelhelper.presentation.detail
 import android.annotation.SuppressLint
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,7 +13,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -22,6 +25,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -50,31 +56,117 @@ import com.google.maps.android.compose.rememberCameraPositionState
 fun DetailScreen(
     title: String,
     onBackClick: () -> Unit,
-    viewModel: DetailViewModel = hiltViewModel()
+    viewModel: DetailViewModel = hiltViewModel(),
+    callTel: (String) -> Unit
 ) {
     val destinationDetailUiState by viewModel.destinationDetailUiState.collectAsState()
+    val errorState by viewModel.errorState.collectAsState()
+    var showDialog by remember { mutableStateOf(true) }
+    val scrollState = rememberScrollState()
 
     LaunchedEffect(key1 = Unit) {
         viewModel.fetchDestinationDetail(title)
+    }
+
+    val address = when (val state = destinationDetailUiState) {
+        is DestinationDetailUiState.DestinationDetails -> state.destinationDetail.address
+        else -> ""
+    }
+
+    val imageUrl = when (val state = destinationDetailUiState) {
+        is DestinationDetailUiState.DestinationDetails -> state.image[0]
+        else -> ""
     }
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
             TravelHelperTopBar(
-                navigationType = TopAppBarNavigationType.BACK,
-                onNavigationClick = onBackClick
+                navigationType = TopAppBarNavigationType.DETAIL,
+                onNavigationClick = onBackClick,
+                onSaveClick = { viewModel.saveToBookmarkList(title, address, imageUrl) }
             )
         }
     ) {
+        errorState?.let {
+            if(it) {
+                if(showDialog) {
+                    AlertDialog(onDismissRequest = { showDialog = false }) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(230.dp)
+                                .padding(horizontal = 10.dp)
+                                .clip(RoundedCornerShape(16.dp))
+                                .background(Color.White),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Spacer(modifier = Modifier.height(85.dp))
+                            Text(
+                                text = "You already saved it!",
+                            )
+                            Spacer(modifier = Modifier.weight(1f))
+                            Button(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(50.dp)
+                                    .padding(horizontal = 20.dp)
+                                    .clip(RoundedCornerShape(16.dp)),
+                                onClick = { showDialog = false }
+                            ) {
+                                Text(
+                                    text = "OK",
+                                    style = MaterialTheme.typography.bodyLarge
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(15.dp))
+                        }
+                    }
+                }
+            }else {
+                if(showDialog) {
+                    AlertDialog(onDismissRequest = { showDialog = false }) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(230.dp)
+                                .padding(horizontal = 10.dp)
+                                .clip(RoundedCornerShape(16.dp))
+                                .background(Color.White),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Spacer(modifier = Modifier.height(85.dp))
+                            Text(
+                                text = "Saved!",
+                            )
+                            Spacer(modifier = Modifier.weight(1f))
+                            Button(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(50.dp)
+                                    .padding(horizontal = 20.dp)
+                                    .clip(RoundedCornerShape(16.dp)),
+                                onClick = { showDialog = false }
+                            ) {
+                                Text(
+                                    text = "OK",
+                                    style = MaterialTheme.typography.bodyLarge
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(15.dp))
+                        }
+                    }
+                }
+            }
+        }
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(horizontal = 15.dp),
+                .padding(horizontal = 15.dp)
+                .verticalScroll(scrollState),
         ) {
             Spacer(modifier = Modifier.height(68.dp))
-
-            DetailContent(destinationDetailUiState, onBackClick)
+            DetailContent(destinationDetailUiState, onBackClick, callTel)
         }
     }
 }
@@ -83,7 +175,8 @@ fun DetailScreen(
 @Composable
 fun DetailContent(
     uiState: DestinationDetailUiState,
-    onBackClick: () -> Unit
+    onBackClick: () -> Unit,
+    callTel: (String) -> Unit
 ) {
     when(uiState) {
         is DestinationDetailUiState.DestinationDetails -> {
@@ -103,7 +196,21 @@ fun DetailContent(
                 Spacer(modifier = Modifier.height(13.dp))
                 Information(iconRes = R.drawable.ic_location, description = uiState.destinationDetail.address)
                 Spacer(modifier = Modifier.height(13.dp))
-                Information(iconRes = R.drawable.ic_tel, description = uiState.destinationDetail.tel)
+                Box(
+                    modifier = Modifier
+                        .clickable(
+                            onClick = { callTel(uiState.destinationDetail.tel) }
+                        )
+                ) {
+                    Information(iconRes = R.drawable.ic_tel, description = uiState.destinationDetail.tel)
+                }
+
+                Spacer(modifier = Modifier.height(25.dp))
+                Text(
+                    text = uiState.description,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Spacer(modifier = Modifier.height(25.dp))
                 GoogleMap(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -116,6 +223,7 @@ fun DetailContent(
                         title = uiState.destinationDetail.name
                     )
                 }
+                Spacer(modifier = Modifier.height(25.dp))
             }
         }
         is DestinationDetailUiState.Loading -> {
@@ -164,7 +272,8 @@ fun DetailContent(
 @Composable
 private fun Information(
     @DrawableRes iconRes: Int,
-    description: String
+    description: String,
+    onClick: (String) -> Unit = { }
 ) {
     Row(
         verticalAlignment = Alignment.CenterVertically
