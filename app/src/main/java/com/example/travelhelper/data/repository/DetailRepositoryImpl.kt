004fe56aec52047d5649fbf006871512c654f57a
Seunggu Kang <alex9366@naver.com>
2023-12-15 20:35:01 +0900

    feat: 관광지에 대한 설명을 요청하는 함수 추가

--- app/src/main/java/com/example/travelhelper/data/repository/DetailRepositoryImpl.kt
+++ app/src/main/java/com/example/travelhelper/data/repository/DetailRepositoryImpl.kt
@@ -1,12 +1,17 @@
 package com.example.travelhelper.data.repository
 
-import com.example.travelhelper.data.api.TourApiService
+import com.example.travelhelper.data.remote.GptApiService
+import com.example.travelhelper.data.remote.TourApiService
+import com.example.travelhelper.data.remote.model.ChatGptRequest
+import com.example.travelhelper.data.remote.model.GptResponse
 import com.example.travelhelper.data.mapper.toData
 import com.example.travelhelper.domain.entity.DestinationDetail
 import com.example.travelhelper.domain.repository.DetailRepository
+import javax.inject.Inject
 
-class DetailRepositoryImpl(
-    private val tourApi: TourApiService
+class DetailRepositoryImpl @Inject constructor(
+    private val tourApi: TourApiService,
+    private val gptApi: GptApiService
 ): DetailRepository {
     override suspend fun getDestinationDetail(query: String): DestinationDetail {
         return tourApi.getDestinationDetail(query = query)
@@ -15,5 +20,9 @@ class DetailRepositoryImpl(
             .items
             .item[0]
             .toData()
+    }
+
+    override suspend fun getDestinationInformation(request: ChatGptRequest): GptResponse {
+        return gptApi.getDestinationInformation(request = request)
     }
 }
