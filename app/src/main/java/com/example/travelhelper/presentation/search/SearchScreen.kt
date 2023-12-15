40e2bd442a67203e4bdff59521538390460521dd
Seunggu Kang <alex9366@naver.com>
2023-12-15 20:44:12 +0900

    feat: SearchBar 구현

--- app/src/main/java/com/example/travelhelper/presentation/search/SearchScreen.kt
+++ app/src/main/java/com/example/travelhelper/presentation/search/SearchScreen.kt
@@ -1,18 +1,15 @@
 package com.example.travelhelper.presentation.search
 
 import android.annotation.SuppressLint
-import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SearchBar
-import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,108 +17,62 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
-import com.example.travelhelper.ui.component.TopAppBarNavigationType
-import com.example.travelhelper.ui.component.TravelHelperTopBar
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.TextFieldValue
+import com.example.travelhelper.R
 
-@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun SearchScreen(
     onBackClick: () -> Unit,
+    navigateToDetailScreen: (String) -> Unit
 ) {
+    val focusManager = LocalFocusManager.current
+    val interactionSource = remember { MutableInteractionSource() }
+
     Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        topBar = {
-            TravelHelperTopBar(
-                navigationType = TopAppBarNavigationType.BACK,
-                onNavigationClick = onBackClick
+        modifier = Modifier
+            .fillMaxSize()
+            .clickable(
+                onClick = { focusManager.clearFocus() },
+                indication = null,
+                interactionSource = interactionSource
             )
-        }
     ) {
-        var queryString by remember {
-            mutableStateOf("")
-        }
-
-        // if the search bar is active or not
-        var isActive by remember {
-            mutableStateOf(false)
-        }
-
-        val contextForToast = LocalContext.current.applicationContext
-
-        SearchBar(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = if (isActive) 0.dp else 8.dp),
-            colors = SearchBarDefaults.colors(
-                containerColor = Color.White,
-            ),
-            query = queryString,
-            onQueryChange = { newQueryString ->
-                queryString = newQueryString
-            },
-            onSearch = {
-                isActive = false
-                Toast.makeText(contextForToast, "Your query string: $queryString", Toast.LENGTH_SHORT)
-                    .show()
-            },
-            active = isActive,
-            onActiveChange = { activeChange ->
-                isActive = activeChange
-            },
-            placeholder = {
-                Text(text = "Search")
-            },
-            leadingIcon = {
-                Icon(imageVector = Icons.Default.Search, contentDescription = null)
-            },
-        ) {
-
-        }
+        SearchBar(onBackClick = onBackClick, navigateToDetailScreen = navigateToDetailScreen)
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun searchBar() {
-    var queryString by remember {
-        mutableStateOf("")
-    }
-
-    // if the search bar is active or not
-    var isActive = true
-
-    val contextForToast = LocalContext.current.applicationContext
-
-    SearchBar(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = if (isActive) 0.dp else 8.dp),
-        colors = SearchBarDefaults.colors(
-            containerColor = Color.White,
-        ),
-        query = queryString,
-        onQueryChange = { newQueryString ->
-            queryString = newQueryString
-        },
-        onSearch = {
-            isActive = false
-            Toast.makeText(contextForToast, "Your query string: $queryString", Toast.LENGTH_SHORT)
-                .show()
-        },
-        active = isActive,
-        onActiveChange = { activeChange ->
-            isActive = activeChange
-        },
-        placeholder = {
-            Text(text = "Search")
-        },
-        leadingIcon = {
-            Icon(imageVector = Icons.Default.Search, contentDescription = null)
-        },
-    ) {
-
+private fun SearchBar(
+    onBackClick: () -> Unit,
+    navigateToDetailScreen: (String) -> Unit
+) {
+    Row {
+        var text by remember { mutableStateOf(TextFieldValue("")) }
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            modifier = Modifier.weight(1f),
+            placeholder = { Text("Search") },
+            singleLine = true,
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.White,
+                unfocusedContainerColor = Color.White,
+                disabledContainerColor = Color.White,
+                cursorColor = Color.Black
+            ),
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_search),
+                    contentDescription = "Search Icon",
+                    modifier = Modifier.clickable {
+                        onBackClick()
+                        navigateToDetailScreen(text.text)
+                    }
+                )
+            }
+        )
     }
 }
